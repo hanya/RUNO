d1d23740f512b308c302e3a44eaf9fffbbe56046
Tsutomu Uchino <hanya.runo@gmail.com>
2012-09-07 21:00:00 +0900

    Replace resolver based connector with connector based one

--- src/connector.rb
+++ src/connector.rb
@@ -45,12 +45,11 @@ module Uno
     def self.bootstrap(office="soffice", type="socket", 
          host="localhost", port=2083, pipe_name=nil, nodelay=false)
       url, argument = self.url_construct(type, host, port, pipe_name, nodelay)
-      r = self.resolver_get
       c = nil
       n = 0
       begin
-        c = self.connect(url, r)
-      rescue Uno::UnoException => e
+        c = self.connect(url)
+      rescue Uno::Com::Sun::Star::Uno::Exception => e
         raise e if e.uno_instance_of?(
             Uno::Com::Sun::Star::Connection::ConnectionSetupException)
         n += 1
@@ -62,9 +61,39 @@ module Uno
       return c
     end
     
-    def self.connect(url, resolver=nil)
-      resolver = self.resolver_get unless resolver
-      return resolver.resolve(url)
+    def self.connect(data=nil, bridge_name=nil)
+      if data.kind_of?(String)
+        data = data[4, -1] if data.start_with?('uno:')
+        description, protocol, object = data.split(';')
+      elsif !data.kind_of?(Hash)
+        data = {'type' => 'pipe', 'name' => 'rubypipe', 
+                'protocol' => 'urp', 'object' => 'StarOffice.ComponentContext'}
+        #data = {'type' => 'socket', 'host' => 'localhost', 'port' => 2082, 
+        #        'protocol' => 'urp', 'object' => 'StarOffice.ComponentContext'}
+      end
+      if data.kind_of?(Hash)
+        contype = data.fetch('type', 'pipe')
+        if contype == 'pipe'
+          description = "pipe,name=#{data.fetch('name')}"
+        elsif contype == 'socket'
+          description = "socket,host=#{data.fetch('host')},port=#{data.fetch('port')}"
+        else
+          raise "Illegal connection type is specified #{contype}"
+        end
+        protocol = data.fetch('protocol', 'urp')
+        object = data.fetch('object', 'StarOffice.ComponentContext')
+        bridge_name = data.fetch('bridge_name', bridge_name || "")
+      end
+      bridge_name = bridge_name.to_s
+      local_ctx = Uno.get_component_context
+      local_smgr = local_ctx.getServiceManager
+      connector = local_smgr.createInstanceWithContext(
+                    "com.sun.star.connection.Connector", local_ctx)
+      connection = connector.connect(description)
+      factory = local_smgr.createInstanceWithContext(
+                    "com.sun.star.bridge.BridgeFactory", local_ctx)
+      bridge = factory.createBridge(bridge_name, protocol, connection, nil)
+      return bridge.getInstance(object)
     end
     
     def self.url_construct(type="socket", 
@@ -86,11 +115,6 @@ module Uno
       return url, argument
     end
     
-    def self.resolver_get
-      ctx = Uno.get_component_context
-      return ctx.getServiceManager.createInstanceWithContext(
-                      "com.sun.star.bridge.UnoUrlResolver", ctx)
-    end
   end
 end
 
